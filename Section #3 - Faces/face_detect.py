83eb4896e8a43cef5ca1ece9d65664926de04e39
Chibueze Anyachebelu <168302805+The-RealOG@users.noreply.github.com>
2025-05-19 14:33:55 -0500

    modified face_detect.py

--- Section #3 - Faces/face_detect.py
+++ Section #3 - Faces/face_detect.py
@@ -3,13 +3,16 @@
 import cv2 as cv
 
 img = cv.imread('../Resources/Photos/group 1.jpg')
-cv.imshow('Group of 5 people', img)
+cv.imshow('People', img)
 
+#GrayScale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray People', gray)
 
+# using the Cascade Classofier from the haar_face.xml file
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
+#Displays a rectangle around the detected faces
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
 
 print(f'Number of faces found = {len(faces_rect)}')
@@ -18,7 +21,5 @@ for (x,y,w,h) in faces_rect:
     cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
 
 cv.imshow('Detected Faces', img)
-
-
 
 cv.waitKey(0)
